1b94e65413f948ebfd19d6eff8b1bff9495e1232
fahad <75642792+fahadarshad08@users.noreply.github.com>
2022-05-19 19:36:59 +0500

    compete project

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Header from "./components/Header";
 import Home from "./components/Home";
-
-
 import './App.css';
+import Detail from "./components/Detail";
 
 function App() {
   return (
@@ -14,7 +13,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />}> 
           </Route>
-          <Route path="/Home" element={<Home/>}> 
+          <Route path="/Home" element={<Home />}>
+              <Route path=":id" element={<Home />} />
+          </Route>
+          <Route path="/Detail" element={<Detail />}>
+              <Route path=":id" element={<Detail />} />
           </Route>
         </Routes>
       </Router>
